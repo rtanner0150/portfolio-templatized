8fef485da2c8ce407bd2f51bf50b06e280901037
tannertronic <71511920+tannertronic@users.noreply.github.com>
2020-10-08 13:41:19 -0500

    contact form func testing 2

--- contact.php
+++ contact.php
@@ -1,31 +1,40 @@
 <?php
 
-$errors = ”;
-$myemail = ‘rtanner0150@skilledkc.org’; //<—–Put Your email address here. 
+$errors = "";
 
-if(empty($_POST[‘contact-name’]) || empty($_POST[’contact-email’]) || empty($_POST[‘contact-message’]))
+$myemail = "rtanner0150@skilledkc.org";
+
+if(empty($_POST["contact-name"]) || empty($_POST["contact-email"]) || empty($_POST["contact-message"]))
+
 {
-$errors .= “\n Error: all fields are required”;
+
+$errors .= "\n Error: all fields are required";
+
 }
 
 $name = $_POST[‘contact-name’];
+
 $email_address = $_POST[’contact-email’];
+
 $message = $_POST[‘contact-message’];
 
-if (!preg_match(“/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i”, $email_address))
+if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email_address))
+
 {
-$errors .= “\n Error: Invalid email address”;
+
+$errors .= "\n Error: Invalid email address";
+
 }
 
 if( empty($errors))
 {
 $to = $myemail;
-$email_subject = “Contact form submission: $name”;
-$email_body = “You have received a new message. “.
-” Here are the details:\n Name: $name \n “.
-“Email: $email_address\n Message \n $message”;
-$headers = “From: $myemail\n”;
-$headers .= “Reply-To: $email_address”;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $name \n ".
+"Email: $email_address\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 
 //redirect to the ‘thank you’ page
