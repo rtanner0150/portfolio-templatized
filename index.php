871912cf74f590060fd3b4a9bd15dd71dcdc9add
tannertronic <71511920+tannertronic@users.noreply.github.com>
2020-10-09 08:26:48 -0500

    debugging

--- index.php
+++ index.php
@@ -1,1 +1,241 @@
-<?php include_once("index.html"); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Ryan Tanner's personal portfolio website" />
+        <meta name="author" content="Ryan Tanner" />
+        <title>Ryan Tanner</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/style-custom.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Ryan Tanner</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profile-square.png" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Ryan
+                        <span class="text-highlight">Tanner</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        808 S Park Ave · Independence, MO 64052 · (816) 328-0299 ·
+                        <a href="mailto:theryantanner@gmail.com">theryantanner@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">An aspiring full stack web developer experienced in working as part of a team, agile methodologies, and cross-browser/responsive front-end development.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/ryan-tanner-23b80690/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/rtanner0150"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Delivery Driver</h3>
+                            <div class="subheading mb-3">Papa John's</div>
+                            <p>Deliver pizzas and fulfill customer satisfaction</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">September 2020 - Present</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">General Manager</h3>
+                            <div class="subheading mb-3">Papa John's</div>
+                            <p>Run store operations, hire and train new employees, manage current employees, write schedules, forecast sales data, manage labor and food costs, manage inventory orders, uphold company quality standards</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">June 2019 - September 2020</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Hourly Shift Leader</h3>
+                            <div class="subheading mb-3">Papa John's</div>
+                            <p>Manage employees, run shifts, take care of customers</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">June 2018 - June 2019</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Delivery Driver</h3>
+                            <div class="subheading mb-3">Papa John's</div>
+                            <p>Deliver pizzas and fulfill customer satisfaction</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">September 2017 - June 2018</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Front-end Web Developer</h3>
+                            <div class="subheading mb-3">iModules Software</div>
+                            <p>Create web templates for client use in a content management system using HTML5, CSS3, LESS, Javascript, jQuery, and Bootstrap</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">October 2014 - July 2017</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Skilled KC Technical Institute</h3>
+                            <div class="subheading mb-3">Software Development Cohort</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">September 2020 - February 2021</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Centriq Training</h3>
+                            <div class="subheading mb-3">Web Development Track</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">January 2014 - May 2014</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Raytown High School</h3>
+                            <div class="subheading mb-3">A.P. Java Programming</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-highlight">August 2005 - May 2009</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-sass"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-less"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p>Aside from my career goals, my biggest passion is music, both listening and playing. I play a handful of instruments and love to sing.</p>
+                    <p class="mb-0">I'm also a big fan of video games, DC Comics, and offbeat humor. I've found that I quite enjoy cooking (and am getting kind of good at it!).</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Projects-->
+            <section class="resume-section" id="projects">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Projects</h2>
+                    <div id="carouselProjects" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <div class="image">
+                                    <img src="https://via.placeholder.com/600x300/333333/eeeeee" class="d-block w-100" alt="Project">
+                                </div>
+                                <div class="text"><a href="project1.html">Project 1</a></div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="image">
+                                    <img src="https://via.placeholder.com/600x300/dddddd/222222" class="d-block w-100" alt="Project">
+                                    <div class="text"><a href="#" target="_blank">Project 2</a></div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="image">
+                                    <img src="https://via.placeholder.com/600x300" class="d-block w-100" alt="Project">
+                                    <div class="text"><a href="#" target="_blank">Project 3</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselProjects" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselProjects" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <div class="contact-wrapper container">
+                <div class="contact-form">
+                    <form action="contact.php" method="post" enctype="text/plain">
+                        <label for="contact-name">Name: <input type="text" id="contact-name" name="contact-name" required></label>
+                        <label for="contact-email">Email: <input type="text" id="contact-email" name="contact-email" required></label>
+                        <label for="contact-message">Message: <textarea id="contact-message" name="contact-message" required></textarea></label>
+                        <hr>
+                        <button class="btn btn-primary" type="submit" id="contact-submit">Submit</button>
+                    </form>
+                </div>
+                <div class="contact-toggle"><h4>Contact Me</h4></div>
+            </div>
+
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
